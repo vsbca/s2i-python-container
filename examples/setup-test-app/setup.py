5859800a0f708cf80e3bde6657215532a59736f1
vsbca <virendra.bisht@outlook.com>
2018-04-20 00:09:18 -0400

    added cython

--- examples/setup-test-app/setup.py
+++ examples/setup-test-app/setup.py
@@ -6,5 +6,5 @@ setup (
     version          = "0.1",
     description      = "Example application to be deployed.",
     packages         = find_packages(),
-    install_requires = ["numpy","pymisp","requests","pandas"],
+    install_requires = ["numpy","pymisp","requests","pandas","cython"],
 )
